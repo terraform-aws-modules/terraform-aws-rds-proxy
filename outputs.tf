2e111751a3b6d6a28ac3c7bf8924ac5dcf07e10e
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-27 15:08:36 -0400

    fix: Update CI configuration files to use latest version (#13)

--- outputs.tf
+++ outputs.tf
@@ -1,69 +1,69 @@
 # RDS Proxy
 output "proxy_id" {
   description = "The ID for the proxy"
-  value       = element(concat(aws_db_proxy.this.*.id, [""]), 0)
+  value       = try(aws_db_proxy.this[0].id, "")
 }
 
 output "proxy_arn" {
   description = "The Amazon Resource Name (ARN) for the proxy"
-  value       = element(concat(aws_db_proxy.this.*.arn, [""]), 0)
+  value       = try(aws_db_proxy.this[0].arn, "")
 }
 
 output "proxy_endpoint" {
   description = "The endpoint that you can use to connect to the proxy"
-  value       = element(concat(aws_db_proxy.this.*.endpoint, [""]), 0)
+  value       = try(aws_db_proxy.this[0].endpoint, "")
 }
 
 # Proxy Default Target Group
 output "proxy_default_target_group_id" {
   description = "The ID for the default target group"
-  value       = element(concat(aws_db_proxy_default_target_group.this.*.id, [""]), 0)
+  value       = try(aws_db_proxy_default_target_group.this[0].id, "")
 }
 
 output "proxy_default_target_group_arn" {
   description = "The Amazon Resource Name (ARN) for the default target group"
-  value       = element(concat(aws_db_proxy_default_target_group.this.*.arn, [""]), 0)
+  value       = try(aws_db_proxy_default_target_group.this[0].arn, "")
 }
 
 output "proxy_default_target_group_name" {
   description = "The name of the default target group"
-  value       = element(concat(aws_db_proxy_default_target_group.this.*.name, [""]), 0)
+  value       = try(aws_db_proxy_default_target_group.this[0].name, "")
 }
 
 # Proxy Target
 output "proxy_target_endpoint" {
   description = "Hostname for the target RDS DB Instance. Only returned for `RDS_INSTANCE` type"
-  value       = element(concat(aws_db_proxy_target.db_instance.*.endpoint, aws_db_proxy_target.db_cluster.*.endpoint, [""]), 0)
+  value       = try(aws_db_proxy_target.db_instance[0].endpoint, aws_db_proxy_target.db_cluster[0].endpoint, "")
 }
 
 output "proxy_target_id" {
   description = "Identifier of `db_proxy_name`, `target_group_name`, target type (e.g. `RDS_INSTANCE` or `TRACKED_CLUSTER`), and resource identifier separated by forward slashes (/)"
-  value       = element(concat(aws_db_proxy_target.db_instance.*.id, aws_db_proxy_target.db_cluster.*.id, [""]), 0)
+  value       = try(aws_db_proxy_target.db_instance[0].id, aws_db_proxy_target.db_cluster[0].id, "")
 }
 
 output "proxy_target_port" {
   description = "Port for the target RDS DB Instance or Aurora DB Cluster"
-  value       = element(concat(aws_db_proxy_target.db_instance.*.port, aws_db_proxy_target.db_cluster.*.port, [""]), 0)
+  value       = try(aws_db_proxy_target.db_instance[0].port, aws_db_proxy_target.db_cluster[0].port, "")
 }
 
 output "proxy_target_rds_resource_id" {
   description = "Identifier representing the DB Instance or DB Cluster target"
-  value       = element(concat(aws_db_proxy_target.db_instance.*.rds_resource_id, aws_db_proxy_target.db_cluster.*.rds_resource_id, [""]), 0)
+  value       = try(aws_db_proxy_target.db_instance[0].rds_resource_id, aws_db_proxy_target.db_cluster[0].rds_resource_id, "")
 }
 
 output "proxy_target_target_arn" {
   description = "Amazon Resource Name (ARN) for the DB instance or DB cluster. Currently not returned by the RDS API"
-  value       = element(concat(aws_db_proxy_target.db_instance.*.target_arn, aws_db_proxy_target.db_cluster.*.target_arn, [""]), 0)
+  value       = try(aws_db_proxy_target.db_instance[0].target_arn, aws_db_proxy_target.db_cluster[0].target_arn, "")
 }
 
 output "proxy_target_tracked_cluster_id" {
   description = "DB Cluster identifier for the DB Instance target. Not returned unless manually importing an RDS_INSTANCE target that is part of a DB Cluster"
-  value       = element(concat(aws_db_proxy_target.db_cluster.*.tracked_cluster_id, [""]), 0)
+  value       = try(aws_db_proxy_target.db_cluster[0].tracked_cluster_id, "")
 }
 
 output "proxy_target_type" {
   description = "Type of target. e.g. `RDS_INSTANCE` or `TRACKED_CLUSTER`"
-  value       = element(concat(aws_db_proxy_target.db_instance.*.type, aws_db_proxy_target.db_cluster.*.type, [""]), 0)
+  value       = try(aws_db_proxy_target.db_instance[0].type, aws_db_proxy_target.db_cluster[0].type, "")
 }
 
 # DB proxy endponts
@@ -75,5 +75,5 @@ output "db_proxy_endpoints" {
 # CloudWatch logs
 output "log_group_arn" {
   description = "The Amazon Resource Name (ARN) of the CloudWatch log group"
-  value       = element(concat(aws_cloudwatch_log_group.this.*.arn, [""]), 0)
+  value       = try(aws_cloudwatch_log_group.this[0].arn, "")
 }
