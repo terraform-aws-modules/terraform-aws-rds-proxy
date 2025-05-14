c93848c9b66b60b200caffb11859af087700f39f
Uladzislau Orlovskiy <painanguishagony@gmail.com>
2025-05-14 09:21:00 +0200

    fix: Correct service principal to rds.amazonaws.com since ints universal across partitions.

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["rds.${data.aws_partition.current.dns_suffix}"]
+      identifiers = ["rds.amazonaws.com"]
     }
   }
 }
